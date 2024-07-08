83d3ed18c2228b6e8500cb5576ea17d7ee5e0060
UpSoftware <info@upsoftware.tech>
2024-07-08 22:11:18 +0200

    1.0.18

--- src/install.js
+++ src/install.js
@@ -4,8 +4,13 @@
  * Docs: https://quasar.dev/app-extensions/development-guide/install-api
  */
 
-
+function installCoreModule(api) {
+	api.extendJsonFile("quasar.extensions.json", {
+		"upsoftware": {},
+	});
+}
 
 export default function (api) {
+	installCoreModule(api);
 	api.compatibleWith('quasar', '^2.0.0')
 }
