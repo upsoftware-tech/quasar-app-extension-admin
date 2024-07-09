8482de4ba1472c981ecef9763c70d8a02a5930f3
UpSoftware <info@upsoftware.tech>
2024-07-09 21:59:34 +0200

    1.0.20

--- src/index.js
+++ src/index.js
@@ -4,28 +4,16 @@
  *
  * Docs: https://quasar.dev/app-extensions/development-guide/index-api
  */
-import { useForm } from "./composables/useForm.js";
-import { useAuth } from "./composables/useAuth.js";
+import {useForm} from "./composables/useForm.js";
+import {useAuth} from "./composables/useAuth.js";
 
 function extendConf (conf, api) {
-	conf.boot.push('~@upsoftware/quasar-app-extension-admin/src/boot/components.js');
-	if (!api.hasVite) {
-		conf.build.transpileDependencies.push(/\\@upsoftware[\\/]quasar-app-extension-admin[\\/]src/);
-		conf.build.transpileDependencies.push(/\\@upsoftware[\\/]admin/);
-	} else {
-		conf.build.vitePlugins = conf.build.vitePlugins || [];
-		conf.build.vitePlugins.push({
-			name: 'quasar-admin-extension',
-			config(config) {
-				config.resolve = config.resolve || {};
-				config.resolve.alias = config.resolve.alias || {};
-				config.resolve.alias['@upsoftware/admin'] = api.resolve.src('');
-			}
-		});
-	}
-	conf.css.push('~@upsoftware/quasar-app-extension-admin/src/assets/scss/components.scss');
+	conf.boot.push('~@upsoftware/quasar-app-extension-admin/src/boot/config.js');
+	
+	conf.framework = conf.framework || {};
+	conf.framework.config = conf.framework.config || {};
 
-	conf.upsoftware = {
+	conf.framework.config.upsoftware = conf.framework.config.upsoftware || {
 		api: {
 			url: 'http://127.0.0.1:8000/',
 			endpoint: {
@@ -38,7 +26,28 @@ function extendConf (conf, api) {
 			register: true,
 			reset: true
 		}
+	};
+
+	// Logowanie, aby upewnić się, że konfiguracja jest ustawiona
+	console.log('conf.framework.config.upsoftware:', conf.framework.config.upsoftware);
+
+
+	conf.boot.push('~@upsoftware/quasar-app-extension-admin/src/boot/components.js');
+	if (!api.hasVite) {
+		conf.build.transpileDependencies.push(/\\@upsoftware[\\/]quasar-app-extension-admin[\\/]src/);
+		conf.build.transpileDependencies.push(/lodash/);
+	} else {
+		conf.build.vitePlugins = conf.build.vitePlugins || [];
+		conf.build.vitePlugins.push({
+			name: 'quasar-admin-extension',
+			config(config) {
+				config.resolve = config.resolve || {};
+				config.resolve.alias = config.resolve.alias || {};
+				config.resolve.alias['@upsoftware/admin'] = api.resolve.src('');
+			}
+		});
 	}
+	conf.css.push('~@upsoftware/quasar-app-extension-admin/src/assets/scss/components.scss');
 }
 
 export default function (api) {
