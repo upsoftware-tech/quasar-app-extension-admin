efb472421920713363ed55e45981ee30947c0f4b
UpSoftware <info@upsoftware.tech>
2024-07-10 00:04:48 +0200

    1.0.25

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,21 +1,15 @@
-import {boot} from 'quasar/wrappers'
-import axios from 'axios'
+import {boot} from 'quasar/wrappers';
+import axios from 'axios';
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
+const defaultApiUrl = 'http://127.0.0.1:8000/v1/';
+
+const api = axios.create({ baseURL: defaultApiUrl });
 
 export default boot(({ app }) => {
-
-	const apiUrl = app.config.globalProperties.$q.config['@upsoftware/admin'].api.url;
-
-	const api = axios.create({ baseURL: apiUrl });
-
-	app.config.globalProperties.$axios = axios
-	app.config.globalProperties.$api = api
+	api.defaults.baseURL = app.config.globalProperties.$q.config['@upsoftware/admin'].api.url || defaultApiUrl;
+	
+	app.config.globalProperties.$axios = axios;
+	app.config.globalProperties.$api = api;
 });
 
-export { api }
+export { api };
